2db3aff5ffc43cec1785611964a8bcebef3b1c14
Light010191 <light010191@yandex.ru>
2024-10-15 23:31:56 +0300

    Добавьте файлы проекта.

---
+++ Application/DTO/Response/ServiceResponse.cs
@@ -0,0 +1,5 @@
+﻿namespace Application.DTO.Response
+{
+	public record ServiceResponse(bool Flag, string Message);
+	
+}
---
+++ Infrastucture/DataAccess/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Application.Extension.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastucture.DataAccess
+{
+	public class AppDbContext(DbContextOptions<AppDbContext> options) : 
+		IdentityDbContext<ApplicationUser>(options)
+	{
+	}
+}
